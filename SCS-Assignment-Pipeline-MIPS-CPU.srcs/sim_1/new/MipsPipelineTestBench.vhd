de5d06efad21262f940895616c9f9ae39df1b33f
keresztesbeata <keresztesbeata00@yahoo.com>
2021-12-10 00:26:38 +0200

    Modified test bench

--- SCS-Assignment-Pipeline-MIPS-CPU.srcs/sim_1/new/MipsPipelineTestBench.vhd
+++ SCS-Assignment-Pipeline-MIPS-CPU.srcs/sim_1/new/MipsPipelineTestBench.vhd
@@ -3,67 +3,42 @@ use IEEE.STD_LOGIC_1164.ALL;
 use work.MipsDefinitions.all;
 
 entity MipsPipelineTestBench is
---  Port ( );
 end MipsPipelineTestBench;
 
 architecture Behavioral of MipsPipelineTestBench is
 component MipsPipeline is
   Port (
-    clk:  in std_logic;
-    btnU:  in std_logic;
-    debug_pc: out std_logic_vector(31 downto 0);
-    debug_branch_addr: out std_logic_vector(31 downto 0);
-    debug_jump_addr: out std_logic_vector(31 downto 0);
-    debug_reg_addr: out std_logic_vector(31 downto 0);
-    debug_A: out std_logic_vector(31 downto 0);
-    debug_B: out std_logic_vector(31 downto 0);
-    debug_immm: out std_logic_vector(31 downto 0);
-    debug_AluResult: out std_logic_vector(31 downto 0);
-    debug_MemWriteData: out std_logic_vector(31 downto 0);
-    debug_MemData: out std_logic_vector(31 downto 0);
-    debug_MemDataInc: out std_logic_vector(31 downto 0);
-    debug_MemDataAdded: out std_logic_vector(31 downto 0);
-    debug_RegWriteAddr: out std_logic_vector(4 downto 0);
-    debug_RegWriteData: out std_logic_vector(31 downto 0)
+    clk:        in std_logic;
+    btnU:       in std_logic;
+    btnD:       in std_logic;
+    btnR:       in std_logic;
+    sw:         in std_logic_vector(15 downto 0);
+    an:         out std_logic_vector(3 downto 0);
+    seg:        out std_logic_vector(6 downto 0);
+    led:        out std_logic_vector(15 downto 0)
   );
 end component;
 
-signal clk:  std_logic;
-signal btnU:  std_logic;
-signal debug_pc: std_logic_vector(31 downto 0);
-signal debug_branch_addr: std_logic_vector(31 downto 0);
-signal debug_jump_addr: std_logic_vector(31 downto 0);
-signal debug_reg_addr: std_logic_vector(31 downto 0);
-signal debug_A: std_logic_vector(31 downto 0);
-signal debug_B: std_logic_vector(31 downto 0);
-signal debug_immm: std_logic_vector(31 downto 0);
-signal debug_AluResult: std_logic_vector(31 downto 0);
-signal debug_MemWriteData: std_logic_vector(31 downto 0);
-signal debug_MemData: std_logic_vector(31 downto 0);
-signal debug_MemDataInc: std_logic_vector(31 downto 0);
-signal debug_MemDataAdded: std_logic_vector(31 downto 0);
-signal debug_RegWriteAddr: std_logic_vector(4 downto 0);
-signal debug_RegWriteData: std_logic_vector(31 downto 0);
+signal clk:        std_logic;
+signal btnU:       std_logic;
+signal btnD:       std_logic;
+signal btnR:       std_logic;
+signal sw:         std_logic_vector(15 downto 0);
+signal an:         std_logic_vector(3 downto 0);
+signal seg:        std_logic_vector(6 downto 0);
+signal led:        std_logic_vector(15 downto 0);
     
 begin
 
 MIPS_UT: MipsPipeline port map(
     clk => clk,
     btnU => btnU,
-    debug_pc => debug_pc,
-    debug_branch_addr => debug_branch_addr,
-    debug_jump_addr => debug_jump_addr,
-    debug_reg_addr => debug_reg_addr,
-    debug_A => debug_A,
-    debug_B => debug_B,
-    debug_immm => debug_immm,
-    debug_AluResult => debug_AluResult,
-    debug_MemWriteData => debug_MemWriteData, 
-    debug_MemData => debug_MemData,
-    debug_MemDataInc => debug_MemDataInc,
-    debug_MemDataAdded => debug_MemDataAdded,
-    debug_RegWriteAddr => debug_RegWriteAddr,
-    debug_RegWriteData => debug_RegWriteData
+    btnD => btnD,
+    btnR => btnR,
+    sw => sw,
+    an => an,
+    seg => seg,
+    led => led
 ); 
 
 process
